50eab330187e07d53b64ae3ffe09b851235cdebe
qwertyczee <kumzak.maty@gmail.com>
2025-05-14 11:17:07 +0200

    first commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+
+const Index = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">Blank Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
